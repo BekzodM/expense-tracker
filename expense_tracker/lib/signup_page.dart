34bfd5086f533b8ba56426b94e8c4f3b9f5ae044
BekzodM <bmusayev07@gmail.com>
2024-07-28 17:16:09 -0400

    Signup Page: Show errors and route after signup.

--- expense_tracker/lib/signup_page.dart
+++ expense_tracker/lib/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/main.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -14,14 +15,14 @@ class SignupPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
-        title: Center(
+        title: const Center(
           child: Text('Sign Up'),
         ),
       ),
       backgroundColor: Colors.indigo.shade400,
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -29,8 +30,8 @@ class SignupPage extends StatelessWidget {
                 width: 500,
                 child: TextField(
                   controller: emailController,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
                       labelText: 'Enter your email',
                       labelStyle: TextStyle(color: Colors.white),
                       enabledBorder: OutlineInputBorder(
@@ -46,8 +47,8 @@ class SignupPage extends StatelessWidget {
                 width: 500,
                 child: TextField(
                   controller: usernameController,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
                       labelText: 'Enter your username',
                       labelStyle: TextStyle(color: Colors.white),
                       enabledBorder: OutlineInputBorder(
@@ -64,8 +65,8 @@ class SignupPage extends StatelessWidget {
                 child: TextField(
                   controller: passwordController,
                   obscureText: true,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
                       labelText: 'Enter your password',
                       labelStyle: TextStyle(color: Colors.white),
                       enabledBorder: OutlineInputBorder(
@@ -76,12 +77,27 @@ class SignupPage extends StatelessWidget {
                       )),
                 ),
               ),
+              const SizedBox(height: 10),
+              const Text(
+                'Password should be at least 6 characters',
+                style: TextStyle(color: Colors.white),
+              ),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () async {
                   String email = emailController.text;
                   String username = usernameController.text;
                   String password = passwordController.text;
+
+                  if (password.length < 6) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Password should be at least 6 characters'),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                    return;
+                  }
 
                   try {
                     UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
@@ -95,10 +111,22 @@ class SignupPage extends StatelessWidget {
                       'email': email,
                       'username': username,
                     });
-
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Successfully signed up!"),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+                    await Future.delayed(const Duration(seconds: 2));
+                    Navigator.push(context,MaterialPageRoute(builder: (context) => const ExpenseTracker()));
                     print('User registered and their data added to Firestore');
                   } catch (e) {
-                    print('Error registering and adding user to Firestore: $e');
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(e.toString()),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
                   }
                 },
                 style: ButtonStyle(
